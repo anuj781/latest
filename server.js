a65bff2be079a4c745e70a6528b07d2707f750ce
anuj781 <anujrana54638@gmail.com>
2025-08-12 08:39:20 +0530

    upp

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.use("/enquiry", enquiryRoutes);
 
 // Serve frontend static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, 'frontend' , 'build')));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, 'frontend' , 'build', 'index.html'))
   );
 } else {
   app.get("/", (req, res) => {
